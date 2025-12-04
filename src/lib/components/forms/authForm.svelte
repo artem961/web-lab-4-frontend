5f57f3a3f30d24959dd25c9816ea2fdf7b0c2f8a
artem961 <artemkhrabrov03@gmail.com>
2025-12-04 05:13:24 +0300

    forms updated

--- src/lib/components/forms/authForm.svelte
+++ src/lib/components/forms/authForm.svelte
@@ -4,35 +4,46 @@
     import Button, { Label } from "@smui/button";
 
     let username = $state("");
-    let password = $state("")
+    let password = $state("");
+    let errorMessage = $state("");
 
-    function loginUser(){
-        let result = login({username: username, password: password});
-        result
-        .then((result) => {
-            let token = result.result?.access_token;
-            let tokenType = result.result?.token_type;
+    function loginUser() {
+        let result = login({ username: username, password: password });
+        result.then((result) => {
+            if (result.result) {
+                let token = result.result.access_token;
+                let tokenType = result.result.token_type;
 
-            localStorage.setItem("access_token", token??"");
-            localStorage.setItem("token_type", tokenType??"");
-        })
-        
+                localStorage.setItem("access_token", token ?? "");
+                localStorage.setItem("token_type", tokenType ?? "");
+                window.location.href = "/canvas"
+            } else if (result.error){
+                errorMessage = result.error.error_message;
+            } else {
+                errorMessage = "Failed to create request"
+            }
+        });
     }
-
 </script>
-
 
 <form>
     <div class="title">
-        <h1>Войти</h1>
+        <h1>Sign in</h1>
     </div>
     <div class="fields">
-        <TextInputField bind:value={username} label="Имя"></TextInputField>
-        <TextInputField bind:value={password} label="Пароль" type="password"></TextInputField>
+        <TextInputField bind:value={username} label="Name"></TextInputField>
+        <TextInputField bind:value={password} label="Password" type="password"
+        ></TextInputField>
+        <p>{errorMessage}</p>
     </div>
     <div class="buttons">
-        <Button variant="raised" onclick={() => {loginUser()}}>
-            <Label>Войти</Label>
+        <Button
+            variant="raised"
+            onclick={() => {
+                loginUser();
+            }}
+        >
+            <Label>Sign in</Label>
         </Button>
     </div>
 </form>
@@ -42,8 +53,7 @@
         align-items: center;
         display: flex;
         flex-direction: column;
-        gap: 2rem
-        
+        gap: 2rem;
     }
 
     .fields {
@@ -54,6 +64,5 @@
     }
 
     .buttons {
-
     }
 </style>
--- src/lib/components/forms/regForm.svelte
+++ src/lib/components/forms/regForm.svelte
@@ -1,27 +1,64 @@
 <script>
-    import { register, login } from "$lib/api/api";
+    import { register } from "$lib/api/api";
     import TextInputField from "./inputs/textInputField.svelte";
     import Button, { Label } from "@smui/button";
 
     let username = $state("");
-    let password = $state("")
-    let passwordRetype = $state("")
+    let password = $state("");
+    let passwordRetype = $state("");
+    let errorMessage = $state("");
 
+    function regUser() {
+        if (username === "") {
+            errorMessage = "Username is empty!";
+        } else if (password !== passwordRetype) {
+            errorMessage = "Passwords doesn't match!";
+        } else if (password.length < 5) {
+            errorMessage =
+                "Your password length should be more then 4 simbols!";
+        } else {
+            let result = register({ username: username, password: password });
+            result.then((result) => {
+                if (result.result) {
+                    let token = result.result.access_token;
+                    let tokenType = result.result.token_type;
+
+                    localStorage.setItem("access_token", token ?? "");
+                    localStorage.setItem("token_type", tokenType ?? "");
+                    window.location.href = "/canvas"
+                } else if (result.error) {
+                    errorMessage = result.error.error_message;
+                } else {
+                    errorMessage = "Failed to create request";
+                }
+            });
+        }
+    }
 </script>
-
 
 <form>
     <div class="title">
-        <h1>Войти</h1>
+        <h1>Registration</h1>
     </div>
     <div class="fields">
-        <TextInputField bind:value={username} label="Имя"></TextInputField>
-        <TextInputField bind:value={password} label="Пароль" type="password"></TextInputField>
-        <TextInputField bind:value={passwordRetype} label="Повторите пароль" type="password"></TextInputField>
+        <TextInputField bind:value={username} label="Name"></TextInputField>
+        <TextInputField bind:value={password} label="Password" type="password"
+        ></TextInputField>
+        <TextInputField
+            bind:value={passwordRetype}
+            label="Retype password"
+            type="password"
+        ></TextInputField>
+        <p>{errorMessage}</p>
     </div>
     <div class="buttons">
-        <Button variant="raised" onclick={() => {register({username: username, password: password})}}>
-            <Label>Войти</Label>
+        <Button
+            variant="raised"
+            onclick={() => {
+                regUser();
+            }}
+        >
+            <Label>Sign up</Label>
         </Button>
     </div>
 </form>
@@ -31,8 +68,7 @@
         align-items: center;
         display: flex;
         flex-direction: column;
-        gap: 2rem
-        
+        gap: 2rem;
     }
 
     .fields {
@@ -43,6 +79,5 @@
     }
 
     .buttons {
-
     }
 </style>
