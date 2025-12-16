264e45a0e7ff4c5417167db3b7af2509e5427de7
artem961 <artemkhrabrov03@gmail.com>
2025-12-16 03:13:13 +0300

    update font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
-	import '$lib/styles/main.scss';
-	import config from '$lib/app-config.json';
-	import duck from '$lib/assets/duck.png';
-    import { FlatToast, ToastContainer } from 'svelte-toasts';
+	import "$lib/styles/main.scss";
+	import config from "$lib/app-config.json";
+	import duck from "$lib/assets/duck.png";
+	import { FlatToast, ToastContainer } from "svelte-toasts";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={duck}/>
+	<link rel="icon" href={duck} />
 	<title>{config.app_name}</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
-<ToastContainer placement={"top-left"} showProgress={true} let:data={data} duration={3500}>
-  <FlatToast {data} />
+<ToastContainer
+	placement={"top-left"}
+	showProgress={true}
+	let:data
+	duration={3500}
+>
+	<FlatToast {data} />
 </ToastContainer>
 
 {@render children()}
